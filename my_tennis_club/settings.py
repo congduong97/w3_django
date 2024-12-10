6e4a584e5464b43ba4c128149aeff691bbd9c7b5
Cong <cong.duong@niteco.se>
2024-12-10 17:47:06 +0700

    Admin config

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c#ta^wllxwn-@7aur6pz&5m@y6^x*p8kadao+n96&qj!d23nu5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
